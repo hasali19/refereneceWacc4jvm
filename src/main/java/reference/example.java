46a58266d277c23f747b532f3509611441d02665
Hasan Ali <hasali20@outlook.com>
2020-02-21 15:40:34 +0000

    Comment out java example

--- src/main/java/reference/example.java
+++ src/main/java/reference/example.java
@@ -1,32 +1,32 @@
 package reference;
 
-import java.io.File;
-import java.util.Optional;
-
-class Main {
-    public static void main(String[] args) {
-        File myWaccProg = new File("./wacc_examples/path/to/my/file.wacc");
-        String stdin = "Some test input for my program to handle";
-        Optional<RefAnswer> answer = ReferenceWACC.compile(myWaccProg, stdin);
-        if (answer.isEmpty()) {
-            System.out.println("Query failed!");
-        } else {
-            int exitCode = answer.get().getCode();
-            String compiledAssembly = answer.get().getAssembly();
-            /* ... */
-        }
-
-        File myAssemblyProg = new File("./path/to/my/assembly.s");
-        Optional<EmulatorReply> reply = ReferenceWACC.emulate(myAssemblyProg, stdin);
-        if (reply.isEmpty()) {
-            System.out.println("Query failed!");
-        } else {
-            String runtimeExitCode = reply.get().getEmulator_exit();
-            String assemblerOut = reply.get().getAssemble_out();
-            String emulatorOutput = reply.get().getEmulator_out();
-            /* ... */
-        }
-
-    }
-}
+// import java.io.File;
+// import java.util.Optional;
+//
+// class Main {
+//     public static void main(String[] args) {
+//         File myWaccProg = new File("./wacc_examples/path/to/my/file.wacc");
+//         String stdin = "Some test input for my program to handle";
+//         Optional<RefAnswer> answer = ReferenceWACC.compile(myWaccProg, stdin);
+//         if (answer.isEmpty()) {
+//             System.out.println("Query failed!");
+//         } else {
+//             int exitCode = answer.get().getCode();
+//             String compiledAssembly = answer.get().getAssembly();
+//             /* ... */
+//         }
+//
+//         File myAssemblyProg = new File("./path/to/my/assembly.s");
+//         Optional<EmulatorReply> reply = ReferenceWACC.emulate(myAssemblyProg, stdin);
+//         if (reply.isEmpty()) {
+//             System.out.println("Query failed!");
+//         } else {
+//             String runtimeExitCode = reply.get().getEmulator_exit();
+//             String assemblerOut = reply.get().getAssemble_out();
+//             String emulatorOutput = reply.get().getEmulator_out();
+//             /* ... */
+//         }
+//
+//     }
+// }
 
